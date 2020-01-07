47b66f17e82260991f1a9ab111e28fb58837c029
wldms111 <852387@naver.com>
2020-01-07 18:06:57 +0900

    vvwww

---
+++ FieldOperationForm/FieldService/User_Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FieldOperationForm
+{
+    public class User_Service
+    {
+        public List<User_Vo> GetCustomerCode()
+        {
+            User_Dac dac = new User_Dac();
+            return dac.GetCustomerCode();
+        }
+    }
+}

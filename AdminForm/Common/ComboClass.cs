c9a42a1bd3a6ebab40a8295d5ecdd7670b78e939
mwkim0402 <mwkim0402@naver.com>
2020-01-21 18:20:18 +0900

    combo만드는중

--- AdminForm/Common/ComboClass.cs
+++ AdminForm/Common/ComboClass.cs
@@ -9,10 +9,23 @@ namespace AdminForm
 {
     public class ComboClass
     {
-        public void ComboBind<T>(T t, ComboBox t)
+        public void ComboBind(List<ComboItem> t, ComboBox combo)
         {
-            
+            Dictionary<string, string> comboList = new Dictionary<string, string>();
+            foreach (var list in t)
+            {
+                comboList.Add(list.comboText, list.comboValue);
+            }
+            combo.DataSource = new BindingSource(comboList, null);
+            combo.DisplayMember = "Value";
+            combo.ValueMember = "Key";                  
         }
 
+    }
+
+    public class ComboItem
+    {
+        public string comboText { get; set; }
+        public string comboValue { get; set; }
     }
 }

27fa37ba99a2acb9cb8985c5ec23102a245279bf
Haechan <rkdgocks@naver.com>
2020-01-31 18:10:59 +0900

    커밋

---
+++ MES_DB/Service/WorkOrderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_DB
+{
+    public class WorkOrderService
+    {
+        public List<WorkReqVo> GetAllWorkReq()
+        {
+            WorkOrderDAC dac = new WorkOrderDAC();
+            return dac.GetWorkReq();
+        }
+        public List<WorkReqCenterVo> GetAllWorkReqCenter(string code)
+        {
+            WorkOrderDAC dac = new WorkOrderDAC();
+            return dac.GetReqCenter(code);
+        }
+    }
+}
---
+++ MES_DB/Vo/WorkOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_DB
+{
+    public class WorkOrder
+    {
+    }
+    public class WorkReqVo
+    {
+        public string Wo_Req_No { get; set; }
+        public int Req_Seq { get; set; }
+        public string Item_Name { get; set; }
+        public int Req_Qty { get; set; }
+        public DateTime Prd_Plan_Date { get; set; }
+        public string Cust_Name { get; set; }
+        public string Sale_Emp { get; set; }
+        public string Req_Status { get; set; }
+    }
+    public class WorkReqCenterVo
+    {
+        public string Workorderbo { get; set; }
+        public DateTime Plan_Date { get; set; }
+        public DateTime Plan_Starttime { get; set; }
+        public DateTime Plan_Endtime { get; set; }
+        public string Wo_Status { get; set; }
+    }
+}
